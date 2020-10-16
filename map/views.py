16d7af7114857fae28c3e12048f1dcf464350694
Caroline Zhou <cz2628@columbia.edu>
2020-10-16 20:07:54 +0000

    Map page created

--- map/views.py
+++ map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from sightings.models import Squirrel
+
 
 # Create your views here.
+
+def map(request):
+    squirrels = Squirrel.objects.all()[:100]
+    context = {'sightings':squirrels}
+    return render(request, 'map/index.html', context)
