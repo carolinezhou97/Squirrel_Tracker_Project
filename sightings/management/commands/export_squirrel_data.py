a2c529a274d35702e76f21295b8c8a88323e6db1
Caroline Zhou <cz2628@columbia.edu>
2020-10-17 22:42:54 +0000

    Add export command

---
+++ sightings/management/commands/export_squirrel_data.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from sightings.models import Squirrel
+
+import pandas as pd
+
+class Command(BaseCommand):
+
+    help='export squirrel dat to csv file to custom path'
+
+    def add_arguments(self,parser):
+        parser.add_argument('path', type=str)
+
+    def handle(self, *args, **kawrgs):
+        path = kwargs['path']
+        df = pd.DataFrame(Squirrel.objects.all(),values())
+        df.to_csv(path)
+
